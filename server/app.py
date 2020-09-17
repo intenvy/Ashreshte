4c0af02095ee6be8b7616377a2922c4daaa660e3
Parsa Saberi <parsa.xmule@gmail.com>
2020-09-17 17:52:06 +0430

    Init

---
+++ server/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
+
+# Init app
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Init db
+db = SQLAlchemy(app)
+
+# Init Ma
+ma = Marshmallow(app)
+
+
+# Models
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = db.Column(db.String(50), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    job = db.Column(db.String(50), nullable=False)
+
+    def __init__(self, name: str, age: int, job: str):
+        self.name = name
+        self.age = age
+        self.job = job
+
+
+# Run Server
+if __name__ == '__main__':
+    app.run(debug=True)
